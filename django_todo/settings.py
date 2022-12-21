8eedea79e1660cf7a51b26dcd73ef104b45dae4e
STANNIDITSANNI <Ditsanstas@gmail.com>
2022-12-21 15:20:40 +0000

    Getting ready for deployment

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -9,11 +9,10 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
 import os
-import dj_database_url
-
-
+if os.path.isfile("env.py"):
+    import env
+    
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
